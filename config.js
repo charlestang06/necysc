67e542abe7b90c2b696d314ec476b9dd15137dc8
Charles Tang <charles06.tang@gmail.com>
2025-01-14 10:20:00 -0500

    generalized config file

---
+++ config.js
@@ -0,0 +1,54 @@
+// config.js
+const CONFIG = {
+    prev_year: 2024,
+    year: 2025,
+    camp_week: "Jul 27 - Aug 2, 2025",
+    staff_camp_week: " Jul 26 - Aug 2, 2025",
+    fees: { day: "$900.00", on: "$1,800.00", ea: "$1,800.00", cit: "$1,100.00" },
+    committee: [
+        ["Christopher Chiu", "Director", "chris_chiu.jpg"],
+        ["Paul Eska", "Assistant Director", "paul_eska.jpg"],
+
+        ["Mikayla Tow", "Head Counselor", "mikayla.jpg"],
+        ["Charles Tang", "Head Counselor", "charles.jpg"],
+
+        ["Fiona Wang", "Assistant Head Counselor", "fiona_wang.jpg"],
+        ["Sydney Chuang", "Assistant Head Counselor", "sydney_chuang.jpg"],
+        ["Olivia Sater", "Assistant Head Counselor", "olivia_sater.jpg"],
+
+        ["David Fox", "Public Relations", "david_fox.jpg"],
+        ["Helen Ng", "Public Relations", "helen_ng.jpg"],
+        ["Andrew Yang", "Public Relations", "andrew_yang.jpg"],
+        ["Kim Yang", "Public Relations", "kim_yang.jpg"],
+        ["Tonia Chu", "Public Relations", "tonia_chu.jpg"],
+        ["Curie Chung", "Public Relations", "curie_chung.jpg"],
+
+        ["Mike Breen", "Registration", "mike_breen.jpg"],
+        ["Barbara O'Toole", "Registration", "barbara_otoole.jpg"],
+        ["Wei Tang", "Registration", "wei_tang.jpg"],
+        ["Hongbo Deng", "Registration", "hongbo_deng.jpg"],
+        ["Rong Yu", "Registration", "rong_yu.jpg"],
+
+        ["Lola Yao", "Treasurer", "lola_yao.jpg"],
+        ["Ivan Yim", "Treasurer", "ivan_yim.jpg"],
+
+        ["Lina Harp", "Education", "lina_harp.jpg"],
+        ["Deb Morris", "Education", "deb_morris.jpg"],
+        ["Eugene Chiu", "Education", "eugene_chiu.jpg"],
+        ["Steven Hsu", "Education", "steven_hsu.jpg"],
+        ["Sandy Chiang", "Education", "sandy_chiang.jpg"],
+        ["Jay Chen", "Education", "jay_chen.jpg"],
+        ["Chingying Hsiao", "Education", "chingying_hsiao.jpg"],
+
+        ["Deb Breen", "General Admin", "deb_breen.jpg"],
+        ["Wendy Chan", "General Admin", "wendy_chan.jpg"],
+        ["Karen Tseng", "General Admin", "karen_tseng.jpg"],
+        ["Gabe Sater", "General Admin", "gabe_sater.jpg"],
+
+
+        ["Danielle Watson", "Medical", "danielle_watson.jpg"],
+        ["Alyssa Chan", "Medical", "alyssa_chan.jpg"],
+        ["Steve Feder", "Medical", "steve_feder.jpg"],
+        ["Jennifer Cheng", "Medical", "jennifer_cheng.jpg"],
+    ],
+};
